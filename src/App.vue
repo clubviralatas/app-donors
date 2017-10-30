b9308a355801786a241851e38ff67d37e95c46de
Ronald Lima <ronaldlima@kofre.com.br>
2017-10-29 23:08:59 -0300

    Color Adjust and router change

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,83 @@
 <template>
   <!-- Don't drop "q-app" class -->
   <div id="q-app">
-    <router-view />
+    <q-layout
+      ref="layout"
+      view="lHh Lpr fff"
+      :left-class="{'bg-red-1': true}"
+      :header-class="{'bg-red-4': true}"
+      :class="{'bg-red-1': true}"
+    >
+      <q-toolbar slot="header" class="glossy bg-red-4">
+        <q-btn
+          flat
+          @click="$refs.layout.toggleLeft()"
+        >
+          <q-icon name="menu"/>
+        </q-btn>
+
+        <q-toolbar-title>
+          Club ViraLatas
+          <div slot="subtitle">Sustentabilidade para ONGs</div>
+        </q-toolbar-title>
+      </q-toolbar>
+
+      <div slot="left">
+        <!--
+          Use <q-side-link> component
+          instead of <q-item> for
+          internal vue-router navigation
+        -->
+
+        <q-list no-border link inset-delimiter>
+          <!--<q-list-header>Opções da Aplicação</q-list-header>-->
+          <q-side-link item to="/">
+            <q-item-side icon="home"/>
+            <q-item-main label="Home" sublabel="Tela de Boas Vindas"/>
+          </q-side-link>
+          <q-side-link item to="/vcards">
+            <q-item-side icon="card membership"/>
+            <q-item-main label="Cartão de Membro" sublabel="Detalhes cartão fidelidade"/>
+          </q-side-link>
+        </q-list>
+      </div>
+      <router-view/>
+    </q-layout>
   </div>
 </template>
 
 <script>
-/*
- * Root component
- */
-export default {}
+  import {
+    QLayout,
+    QToolbar,
+    QToolbarTitle,
+    QBtn,
+    QIcon,
+    QList,
+    QListHeader,
+    QItem,
+    QItemSide,
+    QItemMain,
+    QSideLink
+  } from 'quasar'
+  /*
+   * Root component
+   */
+  export default {
+    components: {
+      QLayout,
+      QToolbar,
+      QToolbarTitle,
+      QBtn,
+      QIcon,
+      QList,
+      QListHeader,
+      QItem,
+      QItemSide,
+      QItemMain,
+      QSideLink
+    }
+  }
 </script>
 
 <style></style>
---
+++ src/components/Vcards.vue
@@ -0,0 +1,89 @@
+<template>
+  <div class="row">
+    <q-card inline>
+      <q-item>
+        <q-item-side avatar="statics/fotoNelci.jpg"/>
+        <q-item-main>
+          <q-item-tile label>Nelci Gomes</q-item-tile>
+          <!--<q-item-tile sublabel>Voluntária ABPA</q-item-tile>-->
+        </q-item-main>
+       <q-chip icon="pets" color="yellow-9">
+          VIP
+        </q-chip>
+      </q-item>
+      <q-card-media>
+        <img src="~assets/fotoMiloca.jpg">
+      </q-card-media>
+      <div class="background-club-viralatas">
+          <q-card-title slot="overlay">
+            <img src="statics/dog.svg" height="32" alt="Dog">
+            <span class="light relative-position" color="">
+            Mila Cachorrinha Linda
+          </span>
+            <span slot="subtitle">Vonlutária ABPA</span>
+          </q-card-title>
+          <q-card-main slot="overlay">
+            Nivel de Doador:
+            <q-rating v-model="ratingModel" :max="5"></q-rating>
+          </q-card-main>
+          <q-card-actions slot="overlay">
+            <q-btn icon="fa-facebook-square" color="primary">Compartilhar</q-btn>
+            <q-btn icon="fa-instagram" color="red">Compartilhar</q-btn>
+          </q-card-actions>
+      </div>
+    </q-card>
+  </div>
+</template>
+
+<script>
+  import {
+    QBtn,
+    QChip,
+    QRating,
+    QCardMedia,
+    QCardTitle,
+    QCard,
+    QCardMain,
+    QItem,
+    QItemMain,
+    QItemTile,
+    QItemSide,
+    QCardActions
+  } from 'quasar'
+
+  export default {
+    components: {
+      QBtn,
+      QChip,
+      QRating,
+      QCardMedia,
+      QCardTitle,
+      QCard,
+      QCardMain,
+      QItem,
+      QItemMain,
+      QItemTile,
+      QItemSide,
+      QCardActions
+    },
+    data () {
+      return {
+        name: 'Nelci Gomes',
+        memberSince: new Date('2017-07-01'),
+        ratingModel: 4
+      }
+    },
+    methods: {}
+  }
+</script>
+
+<style lang="stylus">
+  .background-club-viralatas
+    background-image url(/statics/clubVL-40.png)
+    background-size 115px
+    background-repeat no-repeat
+    background-position right top
+    margin-right 5px
+    margin-top 5px
+
+</style>
